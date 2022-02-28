a4e6f207df07d5684f52cbc9e49bea2ddefefbfa
yalestar <yale.kaul@cambiahealth.com>
2022-02-28 15:27:56 -0800

    Boobus

--- go.mod
+++ go.mod
@@ -3,12 +3,13 @@ module mokker
 go 1.17
 
 require (
+	github.com/aws/aws-sdk-go-v2 v1.13.0
 	github.com/aws/aws-sdk-go-v2/config v1.13.1
 	github.com/aws/aws-sdk-go-v2/service/s3 v1.24.1
+	github.com/bxcodec/faker/v3 v3.7.0
 )
 
 require (
-	github.com/aws/aws-sdk-go-v2 v1.13.0 // indirect
 	github.com/aws/aws-sdk-go-v2/aws/protocol/eventstream v1.2.0 // indirect
 	github.com/aws/aws-sdk-go-v2/credentials v1.8.0 // indirect
 	github.com/aws/aws-sdk-go-v2/feature/ec2/imds v1.10.0 // indirect
@@ -21,5 +22,4 @@ require (
 	github.com/aws/aws-sdk-go-v2/service/sso v1.9.0 // indirect
 	github.com/aws/aws-sdk-go-v2/service/sts v1.14.0 // indirect
 	github.com/aws/smithy-go v1.10.0 // indirect
-	github.com/bxcodec/faker/v3 v3.7.0 // indirect
 )
--- main.go
+++ main.go
@@ -1,77 +1,131 @@
 package main
 
 import (
-    "context"
-    "fmt"
-    "github.com/aws/aws-sdk-go-v2/aws"
-    "github.com/aws/aws-sdk-go-v2/config"
-    "github.com/aws/aws-sdk-go-v2/service/s3"
-    "github.com/bxcodec/faker/v3"
-    "strings"
+	"bufio"
+	"context"
+	"errors"
+	"fmt"
+	v4 "github.com/aws/aws-sdk-go-v2/aws/signer/v4"
+	awshttp "github.com/aws/aws-sdk-go-v2/aws/transport/http"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/service/s3"
 )
 
 // S3CreateBucketAPI defines the interface for the CreateBucket function.
 // We use this interface to test the function using a mocked service.
 type S3CreateBucketAPI interface {
-    CreateBucket(
-        ctx context.Context,
-        params *s3.CreateBucketInput,
-        optFns ...func(*s3.Options),
-    ) (*s3.CreateBucketOutput, error)
+	CreateBucket(
+		ctx context.Context, params *s3.CreateBucketInput, optFns ...func(*s3.Options),
+	) (*s3.CreateBucketOutput, error)
 }
 
-// MakeBucket creates an Amazon Simple Storage Service (Amazon S3) bucket.
-// Inputs:
-//     c is the context of the method call, which includes the AWS Region
-//     api is the interface that defines the method call
-//     input defines the input arguments to the service call.
-// Output:
-//     If success, a CreateBucketOutput object containing the result of the service call and nil.
-//     Otherwise, nil and an error from the call to CreateBucket.
-func MakeBucket(c context.Context, api S3CreateBucketAPI, input *s3.CreateBucketInput,
+func MakeBucket(
+	c context.Context, api S3CreateBucketAPI, input *s3.CreateBucketInput,
 ) (*s3.CreateBucketOutput, error) {
-    return api.CreateBucket(c, input)
+	return api.CreateBucket(c, input)
 }
-gc-b
+
 func main() {
-    cfg, err := config.LoadDefaultConfig(context.TODO(),
-        config.WithEndpointResolverWithOptions(aws.EndpointResolverWithOptionsFunc(
-            func(service, region string, options ...interface{}) (aws.Endpoint, error) {
-                return aws.Endpoint{
-                    URL: "http://localhost:4566",
-                }, nil
-            })),
-    )
-    if err != nil {
-        panic("configuration error, " + err.Error())
-    }
-    client := s3.NewFromConfig(cfg,
-        func(options *s3.Options) {
-            options.UsePathStyle = true
-            options.EndpointOptions.DisableHTTPS = true
-        })
-    input := &s3.ListBucketsInput{}
-    newBucketName := strings.ToLower(faker.FirstName())
-    createBucket(*client, newBucketName)
-    listBuckets(*client, *input)
+	cfg, err := config.LoadDefaultConfig(
+		context.TODO(),
+		config.WithEndpointResolverWithOptions(
+			aws.EndpointResolverWithOptionsFunc(
+				func(service, region string, options ...interface{}) (
+					aws.Endpoint, error,
+				) {
+					return aws.Endpoint{
+						URL: "http://localhost:4566",
+					}, nil
+				},
+			),
+		),
+	)
+	if err != nil {
+		log.Fatal("configuration error, " + err.Error())
+	}
+
+	client := s3.NewFromConfig(
+		cfg,
+		func(options *s3.Options) {
+			options.UsePathStyle = true
+			options.EndpointOptions.DisableHTTPS = true
+		},
+	)
+	input := &s3.ListBucketsInput{}
+	// newBukit := strings.ToLower(faker.FirstName())
+	newBukit := "hoobyak"
+	exists, err := bucketExists(*client, newBukit)
+	if !exists {
+		createBucket(*client, newBukit)
+	}
+	listBuckets(*client, *input)
+	thing, err := putObjectS3(*client, newBukit)
+	if err != nil {
+		log.Println(err)
+	}
+	fmt.Println(thing)
+}
+
+func putObjectS3(client s3.Client, bucket string) (string, error) {
+	dookFile := "/Users/r622233/dev/EXAMPLES/s3mock/dook.txt"
+	file, _ := os.Open(dookFile)
+	f := bufio.NewReader(file)
+	input := s3.PutObjectInput{
+		Bucket: aws.String(bucket),
+		Body:   f,
+		Key:    aws.String("doogie.txt"),
+	}
+
+	_, err := client.PutObject(
+		context.Background(), &input, s3.WithAPIOptions(
+			v4.SwapComputePayloadSHA256ForUnsignedPayloadMiddleware,
+		),
+	)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return "ok", nil
 }
 
 func listBuckets(client s3.Client, input s3.ListBucketsInput) {
-    buckets, err := client.ListBuckets(context.Background(), &input)
-    if err != nil {
-        fmt.Println("Could not list ")
-    }
-    for i, b := range buckets.Buckets {
-        fmt.Println(i, aws.ToString(b.Name))
-    }
+	buckets, err := client.ListBuckets(context.Background(), &input)
+	if err != nil {
+		fmt.Println("Could not list ")
+	}
+	for i, b := range buckets.Buckets {
+		fmt.Println(i, aws.ToString(b.Name))
+	}
 }
 
 func createBucket(client s3.Client, bucketName string) {
-    cbi := s3.CreateBucketInput{
-        Bucket: aws.String(bucketName),
-    }
-    _, err := client.CreateBucket(context.TODO(), &cbi)
-    if err != nil {
-        fmt.Println("Could not create bucketName " + bucketName)
-    }
+	cbi := s3.CreateBucketInput{
+		Bucket: aws.String(bucketName),
+	}
+	_, err := client.CreateBucket(context.TODO(), &cbi)
+	if err != nil {
+		fmt.Println("Could not create bucketName " + bucketName)
+		log.Fatal(err)
+	}
+}
+
+func bucketExists(client s3.Client, bucketName string) (bool, error) {
+	_, err := client.HeadBucket(
+		context.TODO(), &s3.HeadBucketInput{
+			Bucket: aws.String(bucketName),
+		},
+	)
+	if err != nil {
+		var respError *awshttp.ResponseError
+		if errors.As(err, &respError) && respError.ResponseError.HTTPStatusCode() == http.StatusNotFound {
+			return false, nil
+		}
+		return false, err
+	}
+	return true, nil
 }
